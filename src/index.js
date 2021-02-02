a8b1516ec083b7b292046b0d33eb71bfde9d97f7
IvanZhuro <ivan.zhuro.by@gmail.com>
2021-02-02 14:36:11 +0300

    init: start towel-shot task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let matrixNew = [];
+  if (typeof matrix == 'undefined') {
+    return []
+    
+  } else {  
+
+  for ( let i = 0; i < matrix.length; i++) {
+    if ( i % 2 == 0) {
+      for ( let j = 0; j < matrix[i].length; j++ ) {
+        matrixNew.push(matrix[i][j]);
+      } 
+    } else {
+      for ( let j = matrix[i].length - 1; j > -1; j-- ) {
+        matrixNew.push(matrix[i][j]);
+      }
+    }
+  }
+return matrixNew;
+  }
 }
